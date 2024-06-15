9c1a495cd32d449d6b56c149f64fd209c3d988cd
The-EleetCoder <jaijain1803@gmail.com>
2024-06-16 01:28:21 +0530

    day 5

--- week_2/week_2.5/src/index.js
+++ week_2/week_2.5/src/index.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const express = require("express");
+const { join } = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
+// dummy user
+const user = [
+  {
+    name: "John",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
 
-app.listen(3000,()=>{
-    console.log('listening on port 3000')
+app.get("/", (req, res) => {
+  const johnKidneys = user[0].kidneys;
+  const kidneyCount = johnKidneys.length;
+  const healthyKidneyCount = johnKidneys.filter(
+    (kidney) => (kidney.healthy = true)
+  ).length;
+  const unhealthyKidneyCount = kidneyCount - healthyKidneyCount;
+
+  res.send({
+    kidneyCount,
+    healthyKidneyCount,
+    unhealthyKidneyCount
+  })
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
 });

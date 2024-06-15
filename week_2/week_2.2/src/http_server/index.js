2c07434484e3493383a8e60bd7580542a420527b
The-EleetCoder <jaijain1803@gmail.com>
2024-06-15 19:24:41 +0530

    day 4

--- week_2/week_2.2/src/http_server/index.js
+++ week_2/week_2.2/src/http_server/index.js
@@ -3,8 +3,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/conversations", (req, res) => {
+  console.log(req.body);
+  res.send({
+    message: "Hello bitches!",
+  });
 });
 
 app.listen(port, () => {

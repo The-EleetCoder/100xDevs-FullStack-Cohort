37db0b26629b02412cc84faed1e6428d68687fe6
The-EleetCoder <jaijain1803@gmail.com>
2024-06-16 19:32:17 +0530

    day 5

---
+++ week_03/week_3.1/src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const { username, password } = req.headers;
+  const kidneyId = req.query.kidneyId;
+
+  if (username !== "Harkirat" || password !== "1234") {
+    return res.status(400).json({
+      message: "Invalid username or password",
+    });
+  }
+
+  if (kidneyId !== 1 || kidneyId != 2) {
+    return res.status(400).json({
+      message: "Invalid KidneyId",
+    });
+  }
+
+  res.json({
+    message: "Your kidney is healthy!",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});

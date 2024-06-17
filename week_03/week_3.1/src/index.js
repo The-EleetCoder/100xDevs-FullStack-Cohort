1869217674ec5ca37f91eeec6cc7765d9de0fc3f
The-EleetCoder <jaijain1803@gmail.com>
2024-06-17 17:42:39 +0530

    day 6

--- week_03/week_3.1/src/index.js
+++ week_03/week_3.1/src/index.js
@@ -4,32 +4,34 @@ const z = require("zod");
 const app = express();
 app.use(express.json());
 
-// const userMiddleware = (req, res, next) => {
-//   const { username, password } = req.headers;
-//   if (username !== "Harkirat" || password !== "1234") {
-//     return res.status(400).json({
-//       message: "Invalid username or password",
-//     });
-//   }
-//   next();
-// };
+// creating middlewares
+const userMiddleware = (req, res, next) => {
+  const { username, password } = req.headers;
+  if (username !== "Harkirat" || password !== "1234") {
+    return res.status(400).json({
+      message: "Invalid username or password",
+    });
+  }
+  next();
+};
 
-// const kidneyIdMiddleware = (req, res, next) => {
-//   const kidneyId = req.query.kidneyId;
-//   if (kidneyId !== "1" && kidneyId != "2") {
-//     return res.status(400).json({
-//       message: "Invalid KidneyId",
-//     });
-//   }
-//   next();
-// };
+const kidneyIdMiddleware = (req, res, next) => {
+  const kidneyId = req.query.kidneyId;
+  if (kidneyId !== "1" && kidneyId != "2") {
+    return res.status(400).json({
+      message: "Invalid KidneyId",
+    });
+  }
+  next();
+};
 
-// app.get("/health-checkup", userMiddleware, kidneyIdMiddleware, (req, res) => {
-//   res.json({
-//     message: "Your kidney is healthy!",
-//   });
-// });
+app.get("/health-checkup", userMiddleware, kidneyIdMiddleware, (req, res) => {
+  res.json({
+    message: "Your kidney is healthy!",
+  });
+});
 
+// zod validation
 app.post("/health-checkup", (req, res) => {
   const schema = z.array(z.number());
   const { kidneys } = req.body;

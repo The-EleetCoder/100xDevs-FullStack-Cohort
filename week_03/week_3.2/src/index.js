bc4b4abba4b25379f0ead6e9f1b3d9a388e14f49
The-EleetCoder <jaijain1803@gmail.com>
2024-06-18 00:22:37 +0530

    day 7

--- week_03/week_3.2/src/index.js
+++ week_03/week_3.2/src/index.js
@@ -16,27 +16,8 @@ const User = mongoose.model("User", {
 const app = express();
 app.use(express.json());
 
-const ALL_USERS = [
-  {
-    username: "harkirat@gmail.com",
-    password: "123",
-    name: "harkirat singh",
-  },
-  {
-    username: "raman@gmail.com",
-    password: "123321",
-    name: "Raman singh",
-  },
-  {
-    username: "priya@gmail.com",
-    password: "123321",
-    name: "Priya kumari",
-  },
-];
-
 app.post("/signup", async (req, res) => {
   const { name, username, password } = req.body;
-
   const existingUser = await User.findOne({ username: username });
 
   if (existingUser) {
@@ -60,8 +41,8 @@ app.post("/signup", async (req, res) => {
 
 app.post("/signin", async (req, res) => {
   const { username } = req.body;
-
   const existingUser = await User.findOne({ username });
+
   if (!existingUser) {
     return res.status(400).json({
       message: "User doesn't exist!",
@@ -76,6 +57,7 @@ app.post("/signin", async (req, res) => {
 
 app.get("/users", async function (req, res) {
   const token = req.headers.authorization;
+
   try {
     const decoded = jwt.verify(token, jwtPassword);
     const username = decoded.username;
